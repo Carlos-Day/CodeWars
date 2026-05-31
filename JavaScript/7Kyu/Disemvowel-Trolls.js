ae26134765a80c9be284e1c66ab078cac2f10c98
Carlos Day <carlos.day@icloud.com>
2026-05-31 16:21:05 -0700

    Challenge added: Disemvowel Trolls

---
+++ JavaScript/7Kyu/Disemvowel-Trolls.js
@@ -0,0 +1,3 @@
+function disemvowel(str) {
+    return str.replaceAll('a', '').replaceAll('e', '').replaceAll('i','').replaceAll('o','').replaceAll('u','').replaceAll('A', '').replaceAll('E', '').replaceAll('I','').replaceAll('O','').replaceAll('U','')
+  }
